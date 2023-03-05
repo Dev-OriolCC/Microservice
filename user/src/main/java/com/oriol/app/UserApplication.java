21d88f8bc0114d17f975039b567c6f211511d4b6
Dev-OriolCC <cesaroriolcortezz@gmail.com>
2023-03-04 23:22:10 -0500

    microservice creation

---
+++ user/src/main/java/com/oriol/app/UserApplication.java
@@ -0,0 +1,12 @@
+package com.oriol.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+        // User Application
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
